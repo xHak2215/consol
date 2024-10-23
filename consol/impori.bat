8a4bb15ff25b4f115c5481bbe6f9804a0db6caae
xHak2215 <147624393+xHak2215@users.noreply.github.com>
2024-10-23 21:44:40 +0300

    Update impori.bat

--- consol/impori.bat
+++ consol/impori.bat
@@ -1,23 +1,10 @@
-rem updeit
 pip  install pip
 pip install setuptools
 pip install pandas
-rem install
 pip install psutil
 pip install requests
-pip install tabulate
-pip install you-get
-pip install wmi
-pip install asyncio
-pip install wikipedia
-pip install beautifulsoup4
-pip install tkinter-page
 pip install requests
-pip install PyQt5
-pip install getch
 pip install subprocess.run
-pip install QtGui
-pip install QtWidgets
 pip install sockets
-pip install folium
 pip install pyfiglet
+ 
