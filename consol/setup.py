a3ba826d35c97de3bcb34eb83092164484e96b10
xHak2215 <147624393+xHak2215@users.noreply.github.com>
2024-10-23 20:35:34 +0300

    Update setup.py

--- consol/setup.py
+++ consol/setup.py
@@ -1,8 +1,8 @@
 import os
 mein_direct=str(os.path.dirname(os.path.abspath(__name__)))
 mein_direct.encode("utf-8")
-os.chdir(mein_direct)
-os.system("pip install -r requirements.txt")
+os.chdir(mein_direct+"\\cd)
+os.system("pip.exe install -r requirements.txt")
 
 
 
