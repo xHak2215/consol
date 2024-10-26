9666d73c6f019a2975ced9533d1ef2304e5c3b6d
xHak2215 <147624393+xHak2215@users.noreply.github.com>
2024-10-26 21:41:41 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+import os
+mein_direct=str(os.path.dirname(os.path.abspath(__name__)))
+mein_direct.encode("utf-8")
+os.chdir(mein_direct+"\\cd")
+os.system("pip.exe install -r requirements.txt")
+
+
+
+
+
