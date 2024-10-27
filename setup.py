8597786f56840981c1e54440030fdaee8ad66242
xHak2215 <147624393+xHak2215@users.noreply.github.com>
2024-10-27 16:59:52 +0300

    Update setup.py
    wiundows or linuks

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 import os
+import platform
+py='python.exe'
+pip="pip3.12.exe install -r requirements.txt"
+if platform.system() =='Linux':
+    py='python3.exe'
+    pip="pip install -r requirements.txt"
 mein_direct=str(os.path.dirname(os.path.abspath(__name__)))
 mein_direct.encode("utf-8")
+os.system(f"{py} -m pip install --upgrade pip")
 os.chdir(mein_direct+"\\cd")
-os.system("pip.exe install -r requirements.txt")
-
-
-
-
+os.system(pip)
 
