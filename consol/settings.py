1658fddadce546e1302e801d3b7046ac81f550ca
xHak2215 <147624393+xHak2215@users.noreply.github.com>
2025-02-06 22:41:23 +0300

    Update settings.py

--- consol/settings.py
+++ consol/settings.py
@@ -1,13 +1,19 @@
 
-#язык (в разрабоьке) ru - русский eng - English;  shitty translation from me and google
+#язык (в разработке) ru - русский eng - English;  shitty translation from me and google
 lang ="ru"
 #directore
 #mein_direct=str(os.path.dirname(os.path.abspath(__name__)))
 #mein_direct=[mein_direct.decode('UTF-8') for arg in args]
 #logs;on-1,off-0
 log_actived=0
-#log saveс
-log_save=0
+#log save
+log_save=0 
+#установочные пакеты install path
+path ={
+    'calk_m' : "https://github.com/xHak2215/calk_m",
+    'pynet'  : "https://github.com/xHak2215/py.net", 
+    
+}
 # custom
 try:
 
@@ -18,5 +24,5 @@ except:
 #consol title
 consoledTitle="consolSH"
 title = 1# 1 - title on 0 - title off
-#среда исполнения SH-мая кансоль cmd-кансоль виндовс 
+#среда исполнения SH-мая консоль cmd-консоль виндовс 
 sreda="SH"
